d5b31b9770a0b4d14371b9b09753319aa6c2a9fc
Muhammad Aman Siddiqui <154512222+M-AmanSiddiqui@users.noreply.github.com>
2024-05-29 00:19:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var data = 0
+function increament() {
+    data++
+    var increament = document.getElementById("am").innerText = data
+    console.log(increament);
+
+}
+var data = 0
+function Decreament() {
+    data--
+    var Decreament = document.getElementById("am").innerText = data
+    console.log(Decreament);
+
+}
+
+
+function Reset() {
+
+    var Reset = document.getElementById("am").innerText = "0"
+        console.log(Reset);
+
+}
